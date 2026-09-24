25e97fc0f8434e66095e60e149cedd15e6c4700b
abadijayacopier <109567275+abadijayacopier@users.noreply.github.com>
2026-09-24 09:24:24 +0700

    Fix src/plugin-main.c for CI build

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -6,11 +6,13 @@ OBS_MODULE_USE_DEFAULT_LOCALE("kdr-running-text-obs", "en-US")
 
 bool obs_module_load(void)
 {
-    kdr_running_text_graphics_init();\n    blog(LOG_INFO, "KDR Running Text for OBS loaded (version %s)", PLUGIN_VERSION);
+    kdr_running_text_graphics_init();
+    blog(LOG_INFO, "KDR Running Text for OBS loaded (version %s)", PLUGIN_VERSION);
     return kdr_running_text_register();
 }
 
 void obs_module_unload(void)
 {
-    kdr_running_text_graphics_shutdown();\n    blog(LOG_INFO, "KDR Running Text for OBS unloaded");
+    kdr_running_text_graphics_shutdown();
+    blog(LOG_INFO, "KDR Running Text for OBS unloaded");
 }
