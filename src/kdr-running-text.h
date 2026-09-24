6a12e18b451c32a22ec1cfcc18bbe44c68757798
abadijayacopier <109567275+abadijayacopier@users.noreply.github.com>
2026-09-24 09:19:57 +0700

    Prepare native graphics renderer API

--- src/kdr-running-text.h
+++ src/kdr-running-text.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#include <stdbool.h>
+#include <stdbool.h>\n#include <stdint.h>
 
-bool kdr_running_text_register(void);
+bool kdr_running_text_register(void);\nvoid kdr_running_text_graphics_init(void);\nvoid kdr_running_text_graphics_shutdown(void);
